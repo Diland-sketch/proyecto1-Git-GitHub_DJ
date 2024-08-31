a83c16ac0fd4e464378fdfa8dcaf5b8e9e080893
DilandLp <dilandlopez10@gmail.com>
2024-08-31 16:35:18 -0500

    Creacion de clases hijas Cliente, Vendedor y sus metodos

---
+++ src/Cliente.java
@@ -0,0 +1,19 @@
+public class Cliente extends Persona {
+
+    private double saldoPendiete;
+
+    public Cliente(String nombre, String apellido, int identifacion, double saldoPendiete) {
+        super(nombre, apellido, identifacion);
+        this.saldoPendiete = saldoPendiete;
+    }
+
+    public double getSaldoPendiete() {
+        return saldoPendiete;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nSaldoPendiete = " + saldoPendiete;
+    }
+}
